194d7e981e684d97d9ec07de2ded3614f51dd689
Denise Fernandez <dcbfernandez@gmail.com>
2014-01-16 23:56:20 +1100

    Updating (currently) hard coded sponsor info

--- app/models/Sponsor.scala
+++ app/models/Sponsor.scala
@@ -8,8 +8,8 @@ object Sponsor {
   //hardcoded list for now until we integrate with a data store
   def all(): List[Sponsor] = List(
     Sponsor(1, 1, "Google", "img/google.jpg", "www.google.com.au"),
-    Sponsor(2, 1, "Atlassian", "img/google.jpg", "www.atlassian.com"),
-    Sponsor(3, 1, "Github", "img/google.jpg", "www.github.com"),
-    Sponsor(4, 1, "Mi9", "img/google.jpg", "www.google.com.au")
+    Sponsor(2, 1, "Atlassian", "img/atlassian-logo.jpeg", "www.atlassian.com"),
+    Sponsor(3, 1, "Github", "img/github-logo.jpeg", "www.github.com"),
+    Sponsor(4, 1, "Microsoft", "img/microsoft.jpeg", "www.microsoft.com.au")
   )
 }
