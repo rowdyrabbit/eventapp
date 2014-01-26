ff664b7a3940177479ccd43fb607d7b949f48f33
Georgi Knox <georgiknox@gmail.com>
2014-01-26 13:22:30 +1100

    Updated menu service to pull from database

--- app/controllers/Menu.scala
+++ app/controllers/Menu.scala
@@ -1,7 +1,28 @@
 package controllers
 
-import play.api.mvc.Controller
+import play.api.mvc._
+import play.api.libs.json.Json
+import play.api.libs.concurrent.Execution.Implicits._
+import scala.concurrent.Future
+import play.modules.reactivemongo.MongoController
+import play.modules.reactivemongo.json.collection.JSONCollection
+import reactivemongo.api.Cursor
+import models.MenuItem
+import models.MenuItemJsonFormats._
 
-object Menu extends Controller {
-  def list = TODO
+object Menu extends Controller with MongoController {
+
+  def collection: JSONCollection = db.collection[JSONCollection]("menuItems")
+
+  def list = Action.async {
+    val cursor: Cursor[MenuItem] = collection.
+      find(Json.obj()).
+      cursor[MenuItem]
+
+    val futureMenuList: Future[List[MenuItem]] = cursor.collect[List]()
+
+    futureMenuList.map { menuItems =>
+      Ok(Json.toJson(menuItems)).withHeaders("Access-Control-Allow-Origin" -> "*")
+    }
+  }
 }
