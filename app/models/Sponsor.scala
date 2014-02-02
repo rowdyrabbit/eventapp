d433393c877cc7d9218b50ea4d8b9678ebe10320
Georgi Knox <georgiknox@gmail.com>
2014-02-02 19:21:35 +1100

    Updated web services to have order value

--- app/models/Sponsor.scala
+++ app/models/Sponsor.scala
@@ -1,6 +1,6 @@
 package models
 
-case class Sponsor(name: String, src: String, link: String)
+case class Sponsor(name: String, src: String, link: String, order: Int)
 
 object SponsorJsonFormats {
   import play.api.libs.json.Json
